4cf9abe03dc079b552cd49fa5e1fa22beb2b34ab
ht2390 <ht2390@columbia.edu>
2021-11-19 14:55:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime as dt
+import os
+
+import numpy as np
+
+import pandas as pd
+import matplotlib as plt
+
+from util import get_data, plot_data
+
+
+def author():
+    return 'your name here please'
+
+def main():
+    """ Main program """
+    # Code goes over here.
+    return 0
+
+if __name__ == "__main__":
+    main()
